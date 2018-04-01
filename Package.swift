9bf0ce47cb17480973b650d7a0a6052f72bd5735
Nagasawa Hiroki <pixyzehn@icloud.com>
2018-04-01 16:04:23 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Snippet",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.1.0")
+        .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.2.0")
     ],
     targets: [
         .target(
